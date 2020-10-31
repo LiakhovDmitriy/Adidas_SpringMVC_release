78a88eee1febb26a9674362a24b9d97491edd565
LiakhovDmitriy <dimaliahov@gmail.com>
2020-10-31 17:06:10 +0200

    Initial commit!

--- src/main/java/com/gmail/dimaliahov/controller/AdminController.java
+++ src/main/java/com/gmail/dimaliahov/controller/AdminController.java
@@ -1,8 +1,6 @@
 package com.gmail.dimaliahov.controller;
 
 import com.gmail.dimaliahov.model.Product;
-import com.gmail.dimaliahov.model.User;
-import com.gmail.dimaliahov.service.ProductServiceDAOImpl;
 import com.gmail.dimaliahov.serviceForController.serviceInterface.AdminControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,17 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
 
 @Controller
 public class AdminController {
 
 	@Autowired
-	private  AdminControllerService adminControllerService;
+	private AdminControllerService adminControllerService;
 
 	@RequestMapping (value = "/admin", method = RequestMethod.GET)
 	public String getAdmin (HttpSession session, ModelMap modelMap) {
-		return adminControllerService.getAdmin(session,modelMap);
+		return adminControllerService.getAdmin(session, modelMap);
 	}
 
 	@RequestMapping (value = "/admin", method = RequestMethod.POST)
